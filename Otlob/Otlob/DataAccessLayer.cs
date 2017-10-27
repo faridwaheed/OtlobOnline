e9fdade1cd5bf6548ec80d877a2117c7d704c6d7
fawaheed <faridwaheed@hotmail.com>
2017-10-27 18:38:31 +0200

    refs #1-Structure

---
+++ Otlob/Otlob/DataAccessLayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Otlob
+{
+    public class DataAccessLayer
+    {
+        DBEntities DB = new DBEntities();
+
+        public List<Country> GetCountries()
+        {
+            return DB.Countries.ToList();
+        }
+        public List<State> GetStates()
+        {
+            return DB.States.ToList();
+        }
+
+        public List<State> GetStatesByCountryID(int CountryID)
+        {
+            DB.Configuration.ProxyCreationEnabled = false;
+            return DB.States.Where(p => p.country_id == CountryID).ToList();
+        }
+
+        public List<Pharmacy> GetPharamaciesByStateID(int StateID)
+        {
+            DB.Configuration.ProxyCreationEnabled = false;
+            return DB.Pharmacies.Where(p => p.state_id == StateID).ToList();
+        }
+    }
+
+}
